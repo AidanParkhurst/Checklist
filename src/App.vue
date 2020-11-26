f680186504ed921f77c4686d3a9f0ebe5ac4944e
Aidan Parkhurst <aidanparkhurst@gmail.com>
2020-11-26 02:15:37 -0500

    Basic website functionality

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang='pug'>
+h1 What have you done today?
+div(id='taskbar')
+  Task(iconPath='src/assets/lifting.png')
+  Task(iconPath='src/assets/college.png')
+  Task(iconPath='src/assets/MIT.png')
+</template>
+
+<script>
+import Task from './components/Task.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Task
+  }
+}
+</script>
+
+<style scoped>
+#taskbar {
+  width: 46%;
+  margin: 0 auto;
+  overflow: auto;
+}
+h1 {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 2em;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,53 @@
+<template lang='pug'>
+div(id='card' :class='{ completed : isCompleted }')
+  img(:src='iconPath' draggable='false' @click='toggleCompleted()')
+  figcaption {{ caption }}
+</template>
+
+<script>
+export default {
+  props: {
+    iconPath: String,
+    caption: String
+  },
+
+  data() {
+    return {
+      isCompleted: false,
+    }
+  },
+  
+  methods: {
+    toggleCompleted() {
+      this.isCompleted = !this.isCompleted;
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 14em;
+}
+#card {
+  background-color: #EEEEEE;
+  border: 1px solid #CCCCCC;
+  border-radius: 1em;
+  box-shadow: 2px 2px 5px grey;
+  cursor: pointer;
+  float: left;
+  margin: 1em;
+  text-align: center;
+  width: 16em;
+}
+#card:hover {
+  background-color: #DDDDDD;
+}
+
+#card.completed {
+  background-color: #88D969;
+}
+#card.completed:hover {
+  background-color: #77C858;
+}
+</style>
